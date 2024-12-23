0dc5336c6c296991b5016f9e54842c654e85d0ef
Jiaming <zhuge@unlv.nevada.edu>
2024-12-22 20:54:46 -0800

    correct typo

--- Python_scripts/config.py
+++ Python_scripts/config.py
@@ -46,10 +46,10 @@ C_LIGHT = const.c
 G_NEWTON = const.G
 M_PROTON = const.m_p
 
-KM_2_MPC = 3.240779-20 # 3.24e-20 
+KM_2_MPC = 3.240779e-20 # 3.24e-20 
 HUBBLE_TIME = HUBBLE*KM_2_MPC  # Hubble in 1/seconds
 
-DM_2_PCCM3 = 3.240779-23 # 3.24e-23  # Multiply with this to get from the SI result (1/m**2) to pc/cm^3
+DM_2_PCCM3 = 3.240779e-23 # 3.24e-23  # Multiply with this to get from the SI result (1/m**2) to pc/cm^3
 MPC_2_M = 3.085678e+22 # 3.086e+22  
 
 SNR_GW = 8
