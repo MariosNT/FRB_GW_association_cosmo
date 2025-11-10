normalization fix

Notebooks/GWs_FRB_cosmo/Cosmo_constraints_DM_diff_MCMC.py
@@ -30,7 +30,7 @@ w0 = -1.0
 # MCMC parameters
 N_WALKERS = 96
 HEATING = 100
-N_STEPS = 2000
+N_STEPS = 4000
 
 # checkpoint
 RESUME = True
@@ -218,6 +218,24 @@ else:
     
     print(f"Data saved to {SAVE_FILE}")
 
+fig = plt.figure(figsize=(11, 5))
+ax1 = fig.add_subplot(121)
+ax2 = fig.add_subplot(122)
+
+ax1.plot(np.sort(z_centre), np.sort(dL_centre))
+ax1.errorbar(z_centre, dL_obs_centre, yerr=sigma_dL, marker='o', ls='', ms=3, c='r', label=f'N={len(z_centre)}')
+ax1.set_ylabel(r'$d_{L}$ [Mpc]')
+ax1.set_xlabel(r'$z$')
+ax1.legend(loc='upper left')
+
+ax2.plot(np.sort(z_centre), np.sort(DM_centre))
+ax2.errorbar(z_centre, DM_obs_centre, yerr=s_DM_obs, marker='o', ls='', ms=3, c='r')
+ax2.set_ylabel(r'$DM_{\rm diff}$ [pc/cm$^3$]')
+ax2.set_xlabel(r'$z$')
+
+plt.savefig('./plot/data_cluster_DM_diff.png')
+# plt.tight_layout()
+
 #######################
 ### MCMC Analysis ###
 #######################
@@ -270,11 +288,12 @@ def log_likelihood(theta, zs, dLs, s_dLs, DMs, s_DMs):
                 else:
                     p_DM[idx_z]=pdf_DM_cosmo(Delta=Delta, C_0=C0, A=A, sigma=sigma_diff, alpha=3, beta=3)/DM_th """
             
-            p_DM=normalise(p_DM, x_array=z_array)
+            # p_DM=normalise(p_DM, x_array=z_array)
             # p_dL=normalise(GW_dL_kde(lum_distance), x_array=z_array)
-            p_dL=normalise(p_dL, x_array=z_array)
+            # p_dL=normalise(p_dL, x_array=z_array)
             # prob = np.trapz(p_selection*p_dL*p_DM, z_array)
-            integrand = p_selection * p_dL * p_DM
+            p_event = normalise(p_dL * p_DM, z_array)
+            integrand = p_selection * p_event
             prob = np.trapz(integrand, z_array)
             
             if prob > 1e-300:
@@ -650,4 +669,4 @@ if __name__ == '__main__':
     # Save samples to file for later analysis if needed
     np.save('./posterior/GW_FRB_MCMC_DM_diff.npy', samples)
 
-    mcmc_plot_results(samples, param_names, savetitle='MCMC_DM_diff')
+    mcmc_plot_results(samples, param_names, savetitle='./plot/MCMC_cluster_DM_diff')

Python_scripts/cosmo_support.py
@@ -1292,7 +1292,7 @@ def DM_diff_sampling(z, # redshift
     else:
         if (HOF is None):
             raise ValueError("HOF must be provided when not using standard mode.")
-        DM_th=DM_diff_HOf_fast(z, HOF, Om=Om, w=w)
+        DM_th=DM_diff_HOf(z, HOF, Om=Om, w=w)
         
     error=f_variance_delta(S=S, z=z, Om=Om, w=w)
     s_DM_obs = error*DM_th
@@ -1400,7 +1400,8 @@ def DM_ext_sampling(z, # redshift
     Sampling DM_ext for a given redshift and cosmology.
     """
     DM_th=DM_diff_HOf(z, HOF, Om=Om, w=w)
-    dm_range=np.linspace(0.01, 200+5*DM_th, int_N)
+    dm_range=np.linspace(0.25*DM_th, 1000+2.0*DM_th, int_N)
+    # np.linspace(0.01, 200+5*DM_th, int_N)
     
     p_range=[
         calculate_dm_probability_num_HOf_fast(