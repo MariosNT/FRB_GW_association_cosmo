c9a82971601fefb3b63660ac4bfb13d00dc7c9d6
Jiaming <zhuge@unlv.nevada.edu>
2025-11-13 21:57:29 -0800

    for test in cluster

---
+++ Notebooks/GWs_FRB_cosmo/test_run_GW.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name="test_GW"
+#SBATCH --output="%j.test_GW.out"
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --account=unv116
+#SBATCH --ntasks-per-node=96
+#SBATCH -t 48:00:00
+#SBATCH --mem=50G
+#SBATCH --constraint="lustre"
+
+module purge
+module load shared
+module load slurm
+module load cpu/0.17.3b
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate py310
+
+python3 -W ignore::DeprecationWarning test_DM_diff_only_GW.py
+
+python3 ../../../automail.py -task 'DM_diff'
