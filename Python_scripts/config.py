815f0c7ff015bca0fa866c3e5cffc16bd701f2d7
Jiaming <zhuge@unlv.nevada.edu>
2026-05-13 16:55:47 -0700

    clean

--- Python_scripts/config.py
+++ Python_scripts/config.py
@@ -6,7 +6,7 @@
 import numpy as np
 from numpy import linalg as LA
 from numpy.random import default_rng
-rng = default_rng(seed=100) # 3 # 48 # 68
+rng = default_rng(seed=48) # 68 # 100
 import pandas as pd
 
 from scipy import interpolate
